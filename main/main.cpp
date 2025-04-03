b6ee4777d860a54b51579377d167468225ea6ad0
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-04-03 11:07:48 +0530

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -7,6 +7,18 @@ class Solution
 public:
     int longestMountain(int arr[], int n)
     {
-        // write code for Longest Mountain
+       int longest = 0 ;
+        int increase = 0 , decrease = 0 ;
+        for (int i = 1 ; i < arr.size() ; i++) {
+            if ((decrease && arr[i-1] < arr[i]) || arr[i-1] == arr[i])
+                increase = decrease = 0 ;
+
+            increase += (arr[i-1] < arr[i]);
+            decrease += (arr[i-1] > arr[i]);
+
+            if (increase != 0 && decrease != 0)
+                longest = max(longest , increase + decrease + 1 );
+        }
+        return longest ; 
     }
 };
